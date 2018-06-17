ad78e219f5a5c8cd151d8fa4bf9d5323332f7e12
Tony Langhammer <tony.langhammer@yahoo.de>
2018-06-17 20:19:42 +0200

    Create generated_odd_or_even.js

---
+++ examples/generated_odd_or_even.js
@@ -0,0 +1,47 @@
+const number = Math.round(Math.random()*20);
+
+console.log(`Number is ${number}`);
+
+if (number === 0) {
+	console.log('even');
+} else if (number === 1) {
+	console.log('odd');
+} else if (number === 2) {
+	console.log('even');
+} else if (number === 3) {
+	console.log('odd');
+} else if (number === 4) {
+	console.log('even');
+} else if (number === 5) {
+	console.log('odd');
+} else if (number === 6) {
+	console.log('even');
+} else if (number === 7) {
+	console.log('odd');
+} else if (number === 8) {
+	console.log('even');
+} else if (number === 9) {
+	console.log('odd');
+} else if (number === 10) {
+	console.log('even');
+} else if (number === 11) {
+	console.log('odd');
+} else if (number === 12) {
+	console.log('even');
+} else if (number === 13) {
+	console.log('odd');
+} else if (number === 14) {
+	console.log('even');
+} else if (number === 15) {
+	console.log('odd');
+} else if (number === 16) {
+	console.log('even');
+} else if (number === 17) {
+	console.log('odd');
+} else if (number === 18) {
+	console.log('even');
+} else if (number === 19) {
+	console.log('odd');
+} else if (number === 20) {
+	console.log('even');
+}
