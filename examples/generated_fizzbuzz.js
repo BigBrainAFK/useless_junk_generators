fb26498112ca3fd36a89ff216e10f91b125d0d56
Tony Langhammer <tony.langhammer@yahoo.de>
2018-06-17 20:19:01 +0200

    Create generated_fizzbuzz.js

---
+++ examples/generated_fizzbuzz.js
@@ -0,0 +1,43 @@
+for(let i = 1; i <= 20; i++){
+	if (i === 1) {
+		console.log('1');
+	} else if (i === 2) {
+		console.log('2');
+	} else if (i === 3) {
+		console.log('Fizz');
+	} else if (i === 4) {
+		console.log('4');
+	} else if (i === 5) {
+		console.log('Buzz');
+	} else if (i === 6) {
+		console.log('Fizz');
+	} else if (i === 7) {
+		console.log('7');
+	} else if (i === 8) {
+		console.log('8');
+	} else if (i === 9) {
+		console.log('Fizz');
+	} else if (i === 10) {
+		console.log('Buzz');
+	} else if (i === 11) {
+		console.log('11');
+	} else if (i === 12) {
+		console.log('Fizz');
+	} else if (i === 13) {
+		console.log('13');
+	} else if (i === 14) {
+		console.log('14');
+	} else if (i === 15) {
+		console.log('FizzBuzz');
+	} else if (i === 16) {
+		console.log('16');
+	} else if (i === 17) {
+		console.log('17');
+	} else if (i === 18) {
+		console.log('Fizz');
+	} else if (i === 19) {
+		console.log('19');
+	} else if (i === 20) {
+		console.log('Buzz');
+	}
+}
